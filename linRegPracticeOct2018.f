98a7afedbedf7eb397c9b033c1d0ac2739bbcd40
DU-ds <kjcolins1759@gmail.com>
2018-10-19 15:16:52 -0400

    "Done for the day. " M  linRegPracticeOct2018.f

--- linRegPracticeOct2018.f
+++ linRegPracticeOct2018.f
@@ -1,5 +1,5 @@
       program linReg1
-      implicit none
+      
       integer, parameter :: rp = SELECTED_REAL_KIND(15)
       integer, parameter :: n = 1000
       real (kind = rp) , dimension(n, 4):: x       ! it's a 1000 row, 4 column matrix
@@ -32,7 +32,8 @@ C https://docs.oracle.com/cd/E19957-01/805-4939/6j4m0vnaf/index.html
 C file:///R:/Fortran_WD/FortranCrashCourseSeminar/ReadingArrayData.pdf
       
       x_Tx = matmul(x_T, x)
-      x_Txinv = matinv4( x_Tx )
+      x_Txinv = inv(x_Tx)
+C      x_Txinv = matinv4( x_Tx )
 C      x_Txinv = inv(x_Txinv)
 C      print *, x_Txinv
 ! DGETRF computes an LU factorization of a general M-by-N matrix A
@@ -53,8 +54,81 @@ C       end if
 
 
 
+
+
       write (*,*) "program works"
       end program linReg1
+
+      ! Returns the inverse of a matrix calculated by finding the LU
+      ! decomposition.  Depends on LAPACK.
+      function inv(A) result(Ainv)
+         integer, parameter :: rp = SELECTED_REAL_KIND(15)
+         real(kind = rp), dimension(:,:), intent(in) :: A
+         real(kind = rp), dimension(size(A,1),size(A,2)) :: Ainv
+
+         real(kind = rp), dimension(size(A,1)) :: work  ! work array for LAPACK
+         integer, dimension(size(A,1)) :: ipiv   ! pivot indices
+         integer :: n, info
+
+        ! External procedures defined in LAPACK
+         external DGETRF
+         external DGETRI
+
+        ! Store A in Ainv to prevent it from being overwritten by LAPACK
+         Ainv = A
+         n = size(A,1)
+
+        ! DGETRF computes an LU factorization of a general M-by-N matrix A
+        ! using partial pivoting with row interchanges.
+         call DGETRF(n, n, Ainv, n, ipiv, info)
+
+         if (info /= 0) then
+            stop 'Matrix is numerically singular!'
+         end if
+
+        ! DGETRI computes the inverse of a matrix using the LU factorization
+        ! computed by DGETRF.
+         call DGETRI(n, Ainv, n, ipiv, work, n, info)
+
+         if (info /= 0) then
+            stop 'Matrix inversion failed!'
+         end if
+      end function inv
+
+
+      pure function matinv4(A) result(B)
+         integer, parameter :: rp = SELECTED_REAL_KIND(15)
+         !! Performs a direct calculation of the inverse of a 4×4 matrix.
+         real (kind = rp) , intent(in)  :: A(4,4)   !! Matrix
+         real (kind = rp) , intent(out) :: B(4,4)   !! Inverse matrix
+         real (kind = rp)               :: detinv
+
+         ! Calculate the inverse determinant of the matrix
+         detinv = &
+           1/(A(1,1)*(A(2,2)*(A(3,3)*A(4,4)-A(3,4)*A(4,3))+A(2,3)*(A(3,4)*A(4,2)-A(3,2)*A(4,4))+A(2,4)*(A(3,2)*A(4,3)-A(3,3)*A(4,2)))&
+            - A(1,2)*(A(2,1)*(A(3,3)*A(4,4)-A(3,4)*A(4,3))+A(2,3)*(A(3,4)*A(4,1)-A(3,1)*A(4,4))+A(2,4)*(A(3,1)*A(4,3)-A(3,3)*A(4,1)))&
+            + A(1,3)*(A(2,1)*(A(3,2)*A(4,4)-A(3,4)*A(4,2))+A(2,2)*(A(3,4)*A(4,1)-A(3,1)*A(4,4))+A(2,4)*(A(3,1)*A(4,2)-A(3,2)*A(4,1)))&
+            - A(1,4)*(A(2,1)*(A(3,2)*A(4,3)-A(3,3)*A(4,2))+A(2,2)*(A(3,3)*A(4,1)-A(3,1)*A(4,3))+A(2,3)*(A(3,1)*A(4,2)-A(3,2)*A(4,1))))
+
+         ! Calculate the inverse of the matrix
+         B(1,1) = detinv*(A(2,2)*(A(3,3)*A(4,4)-A(3,4)*A(4,3))+A(2,3)*(A(3,4)*A(4,2)-A(3,2)*A(4,4))+A(2,4)*(A(3,2)*A(4,3)-A(3,3)*A(4,2)))
+         B(2,1) = detinv*(A(2,1)*(A(3,4)*A(4,3)-A(3,3)*A(4,4))+A(2,3)*(A(3,1)*A(4,4)-A(3,4)*A(4,1))+A(2,4)*(A(3,3)*A(4,1)-A(3,1)*A(4,3)))
+         B(3,1) = detinv*(A(2,1)*(A(3,2)*A(4,4)-A(3,4)*A(4,2))+A(2,2)*(A(3,4)*A(4,1)-A(3,1)*A(4,4))+A(2,4)*(A(3,1)*A(4,2)-A(3,2)*A(4,1)))
+         B(4,1) = detinv*(A(2,1)*(A(3,3)*A(4,2)-A(3,2)*A(4,3))+A(2,2)*(A(3,1)*A(4,3)-A(3,3)*A(4,1))+A(2,3)*(A(3,2)*A(4,1)-A(3,1)*A(4,2)))
+         B(1,2) = detinv*(A(1,2)*(A(3,4)*A(4,3)-A(3,3)*A(4,4))+A(1,3)*(A(3,2)*A(4,4)-A(3,4)*A(4,2))+A(1,4)*(A(3,3)*A(4,2)-A(3,2)*A(4,3)))
+         B(2,2) = detinv*(A(1,1)*(A(3,3)*A(4,4)-A(3,4)*A(4,3))+A(1,3)*(A(3,4)*A(4,1)-A(3,1)*A(4,4))+A(1,4)*(A(3,1)*A(4,3)-A(3,3)*A(4,1)))
+         B(3,2) = detinv*(A(1,1)*(A(3,4)*A(4,2)-A(3,2)*A(4,4))+A(1,2)*(A(3,1)*A(4,4)-A(3,4)*A(4,1))+A(1,4)*(A(3,2)*A(4,1)-A(3,1)*A(4,2)))
+         B(4,2) = detinv*(A(1,1)*(A(3,2)*A(4,3)-A(3,3)*A(4,2))+A(1,2)*(A(3,3)*A(4,1)-A(3,1)*A(4,3))+A(1,3)*(A(3,1)*A(4,2)-A(3,2)*A(4,1)))
+         B(1,3) = detinv*(A(1,2)*(A(2,3)*A(4,4)-A(2,4)*A(4,3))+A(1,3)*(A(2,4)*A(4,2)-A(2,2)*A(4,4))+A(1,4)*(A(2,2)*A(4,3)-A(2,3)*A(4,2)))
+         B(2,3) = detinv*(A(1,1)*(A(2,4)*A(4,3)-A(2,3)*A(4,4))+A(1,3)*(A(2,1)*A(4,4)-A(2,4)*A(4,1))+A(1,4)*(A(2,3)*A(4,1)-A(2,1)*A(4,3)))
+         B(3,3) = detinv*(A(1,1)*(A(2,2)*A(4,4)-A(2,4)*A(4,2))+A(1,2)*(A(2,4)*A(4,1)-A(2,1)*A(4,4))+A(1,4)*(A(2,1)*A(4,2)-A(2,2)*A(4,1)))
+         B(4,3) = detinv*(A(1,1)*(A(2,3)*A(4,2)-A(2,2)*A(4,3))+A(1,2)*(A(2,1)*A(4,3)-A(2,3)*A(4,1))+A(1,3)*(A(2,2)*A(4,1)-A(2,1)*A(4,2)))
+         B(1,4) = detinv*(A(1,2)*(A(2,4)*A(3,3)-A(2,3)*A(3,4))+A(1,3)*(A(2,2)*A(3,4)-A(2,4)*A(3,2))+A(1,4)*(A(2,3)*A(3,2)-A(2,2)*A(3,3)))
+         B(2,4) = detinv*(A(1,1)*(A(2,3)*A(3,4)-A(2,4)*A(3,3))+A(1,3)*(A(2,4)*A(3,1)-A(2,1)*A(3,4))+A(1,4)*(A(2,1)*A(3,3)-A(2,3)*A(3,1)))
+         B(3,4) = detinv*(A(1,1)*(A(2,4)*A(3,2)-A(2,2)*A(3,4))+A(1,2)*(A(2,1)*A(3,4)-A(2,4)*A(3,1))+A(1,4)*(A(2,2)*A(3,1)-A(2,1)*A(3,2)))
+         B(4,4) = detinv*(A(1,1)*(A(2,2)*A(3,3)-A(2,3)*A(3,2))+A(1,2)*(A(2,3)*A(3,1)-A(2,1)*A(3,3))+A(1,3)*(A(2,1)*A(3,2)-A(2,2)*A(3,1)))
+       end function
+
 
 
 
